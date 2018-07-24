3bb9b7380f22cf9734118530921f70a0695c88f2
shawnk3 <32775598+shawnk3@users.noreply.github.com>
2018-07-24 11:27:54 -0400

    optinal map

--- js/main.js
+++ js/main.js
@@ -2,11 +2,44 @@
  * Transcend - Main JS
  *
  * ------------------------------------------------------------------- */
+//instantiate the request object
+// <!-- Weather
+// var xhttp,xmlDoc,temp_text, city_txt, temp_x, city_x,i;
+// xhttp = new XMLHttpRequest();
+
+// function makeRequest(){
+  // var input = document.getElementById("input");
+  // var url = "https://api.openweathermap.org/data/2.5/weather?q=" + input.
+  // xhttp.open("GET", url, true);
+  // xhttp.send();
+// }
+
+// send onready call back function
+// <!--
+// xhttp.onreadystatechange = function(){
+  // if(this.readyState == 4 && this.status == 200){
+    // console.log(this.responseText);
+    // console.log(this.responseXML);
+    // xmlDoc = this.responseXML;
+    // temp_txt = "";
+    // temp_x = xmlDoc.getElementbyTagName("temperature");
+    // city_txt= "";
+    // city_x = xmlDoc.getElementbyTagName("city");
+    // console.log(temp_x);
+    // console.log(city_x);
+    // for (i = 0; i < temp_x.length; i++) {
+	  // temp_txt += temp_x[i].attributes[0].nodeValue + " " + temp_x[i].attributes[3].nodeValue;
+	  // }
+	// for (i = 0; i < city_x.length; i++) {
+		// city_txt += city_x[i].attributes[1].nodeValue;
+	// }
+  // document.getElementById("demo").innerHTML = city_txt + ": " + temp_txt;
+// }
 
 (function($) {
 
     "use strict";
-    
+
     var cfg = {
         scrollDuration : 800, // smoothscroll duration
         mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
@@ -23,7 +56,7 @@
    /* Preloader
     * -------------------------------------------------- */
     var clPreloader = function() {
-        
+
         $("html").addClass('cl-preload');
 
         $WIN.on('load', function() {
@@ -31,16 +64,16 @@
             //force page scroll position to top at page refresh
             // $('html, body').animate({ scrollTop: 0 }, 'normal');
 
-            // will first fade out the loading animation 
+            // will first fade out the loading animation
             $("#loader").fadeOut("slow", function() {
                 // will fade out the whole DIV that covers the website.
                 $("#preloader").delay(300).fadeOut("slow");
-            }); 
-            
-            // for hero content animations 
+            });
+
+            // for hero content animations
             $("html").removeClass('cl-preload');
             $("html").addClass('cl-loaded');
-        
+
         });
     };
 
@@ -48,7 +81,7 @@
    /* Menu on Scrolldown
     * ------------------------------------------------------ */
     var clMenuOnScrolldown = function() {
-        
+
         var menuTrigger = $('.header-menu-toggle');
 
         $WIN.on('scroll', function() {
@@ -116,7 +149,7 @@
                 $size = $thumbLink.data('size').split('x'),
                 $width  = $size[0],
                 $height = $size[1];
-        
+
             var item = {
                 src  : $href,
                 w    : $width,
@@ -152,7 +185,7 @@
    /* Stat Counter
     * ------------------------------------------------------ */
     var clStatCount = function() {
-        
+
         var statSection = $(".s-stats"),
             stats = $(".stats__count");
 
@@ -174,7 +207,7 @@
                         });
                     });
 
-                } 
+                }
 
                 // trigger once only
                 this.destroy();
@@ -188,9 +221,9 @@
 
 
    /* Masonry
-    * ---------------------------------------------------- */ 
+    * ---------------------------------------------------- */
     var clMasonryFolio = function () {
-        
+
         var containerBricks = $('.masonry');
 
         containerBricks.imagesLoaded(function () {
@@ -210,7 +243,7 @@
     /* slick slider
      * ------------------------------------------------------ */
     var clSlickSlider = function() {
-        
+
         $('.testimonials__slider').slick({
             arrows: false,
             dots: true,
@@ -235,11 +268,11 @@
    /* Smooth Scrolling
     * ------------------------------------------------------ */
     var clSmoothScroll = function() {
-        
+
         $('.smoothscroll').on('click', function (e) {
             var target = this.hash,
             $target    = $(target);
-            
+
                 e.preventDefault();
                 e.stopPropagation();
 
@@ -262,7 +295,7 @@
    /* Placeholder Plugin Settings
     * ------------------------------------------------------ */
     var clPlaceholder = function() {
-        $('input, textarea, select').placeholder();  
+        $('input, textarea, select').placeholder();
     };
 
 
@@ -272,7 +305,7 @@
 
         $('.alert-box').on('click', '.alert-box__close', function() {
             $(this).parent().fadeOut(500);
-        }); 
+        });
 
     };
 
@@ -280,7 +313,7 @@
    /* Animate On Scroll
     * ------------------------------------------------------ */
     var clAOS = function() {
-        
+
         AOS.init( {
             offset: 200,
             duration: 600,
@@ -296,7 +329,7 @@
    /* AjaxChimp
     * ------------------------------------------------------ */
     var clAjaxChimp = function() {
-        
+
         $('#mc-form').ajaxChimp({
             language: 'es',
             url: cfg.mailChimpURL
@@ -321,7 +354,7 @@
             3: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.',
             4: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.',
             5: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.'
-        } 
+        }
 
     };
 
@@ -329,13 +362,13 @@
    /* Back to Top
     * ------------------------------------------------------ */
     var clBackToTop = function() {
-        
+
         var pxShow  = 500,         // height on which the button will show
         fadeInTime  = 400,         // how slow/fast you want the button to show
         fadeOutTime = 400,         // how slow/fast you want the button to hide
         scrollSpeed = 300,         // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
         goTopButton = $(".cl-go-top")
-        
+
         // Show or hide the sticky footer button
         $(window).on('scroll', function() {
             if ($(window).scrollTop() >= pxShow) {
@@ -350,7 +383,7 @@
    /* Initialize
     * ------------------------------------------------------ */
     (function clInit() {
-        
+
         clPreloader();
         clMenuOnScrolldown();
         clOffCanvas();
@@ -366,5 +399,5 @@
         clBackToTop();
 
     })();
-        
+
 })(jQuery);
